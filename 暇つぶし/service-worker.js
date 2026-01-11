cf19d7265f1ab6ab01e3bfb42990c889ef4f73ff
sh0zy <r202502344qd@jindai.jp>
2026-01-11 20:30:09 +0900

    2026

---
+++ 暇つぶし/service-worker.js
@@ -0,0 +1,20 @@
+const CACHE = "wave-talk-v1";
+const FILES = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(FILES)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
